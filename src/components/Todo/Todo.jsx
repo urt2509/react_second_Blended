a8e39e14f20383448ffbef8d3221db77668bf9d5
Alla <urt2509@gmail.com>
2023-08-27 15:44:38 +0200

    task2 done

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,7 +1,17 @@
 import { Text } from 'components';
-import { TodoWrapper, DeleteButton, EditButton } from './Todo.styled';
-import { RiDeleteBinLine, RiEdit2Line } from 'react-icons/ri';
+import { TodoWrapper, DeleteButton } from './Todo.styled';
+import { RiDeleteBinLine } from 'react-icons/ri';
 
-export const Todo = () => {
-  return <h2>Todo</h2>;
+export const Todo = ({ text, todosNumber, onClickDelete, todoId }) => {
+  return (
+    <TodoWrapper>
+      <Text textAlign="center" marginBottom="20px">
+        TODO #{todosNumber}
+      </Text>
+      <Text>{text}</Text>
+      <DeleteButton type="button" onClick={() => onClickDelete(todoId)}>
+        <RiDeleteBinLine size={24} />
+      </DeleteButton>
+    </TodoWrapper>
+  );
 };
--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,28 +1,56 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
-import { Grid, GridItem, SearchForm, EditForm, Text, Todo } from 'components';
+import { Grid, GridItem, SearchForm, Text, Todo } from 'components';
 
 export class Todos extends Component {
   state = {
     todos: [],
   };
 
+  componentDidMount() {
+    const savedTodos = JSON.parse(window.localStorage.getItem('todos'));
+    if (!savedTodos) {
+      return;
+    }
+    this.setState({ todos: savedTodos });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.todos !== this.state.todos) {
+      window.localStorage.setItem('todos', JSON.stringify(this.state.todos));
+    }
+  }
+
   handleSubmit = newText => {
     const todo = { text: newText, id: nanoid() };
     this.setState(prevState => {
       return { todos: [...prevState.todos, todo] };
     });
-    console.log(this.state.todos);
+  };
+
+  handleDeleteTodo = todoId => {
+    this.setState(prevState => ({
+      todos: prevState.todos.filter(todo => todoId !== todo.id),
+    }));
   };
 
   render() {
+    const { todos } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.handleSubmit} />
+        {todos.length === 0 && <Text>Sorry...There are no any todos</Text>}
         <Grid>
-          <GridItem>
-            <Todo />
-          </GridItem>
+          {todos.map(({ id, text }, idx) => (
+            <GridItem key={id}>
+              <Todo
+                text={text}
+                todosNumber={idx + 1}
+                todoId={id}
+                onClickDelete={this.handleDeleteTodo}
+              />
+            </GridItem>
+          ))}
         </Grid>
       </>
     );
